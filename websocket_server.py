f5c2980c796154f233675b41fa0d084a1ed93825
haresudhan <code@0x6c.dev>
2020-12-15 14:47:04 -0700

    Websocket works

--- websocket_server.py
+++ websocket_server.py
@@ -1,18 +1,14 @@
-#!/usr/bin/env python3
-
 import asyncio
 import websockets
 
 async def server(websocket, path):
-    # Get received data from websocket
-    data = await websocket.recv()
-    print("data:", data)
-    # Send response back to client to acknowledge receiving message
-    await websocket.send("Thanks for your message: " + data)
+    try:
+        data = await websocket.recv()
+        print("data:", data)
+    except websockets.exceptions.ConnectionClosedError:
+        # print("Connection Closed")
+        pass
 
-# Create websocket server
 start_server = websockets.serve(server, "localhost", 7777)
-
-# Start and run websocket server forever
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
